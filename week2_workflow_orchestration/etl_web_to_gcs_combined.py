9ec89eaf08eec3f1a0c9937fa8cc8989a0caea35
Hastings Sibanda <hastingssbnd5@gmail.com>
2023-02-25 11:48:42 +0200

    bug: fix gcs bucket name

--- week2_workflow_orchestration/etl_web_to_gcs_combined.py
+++ week2_workflow_orchestration/etl_web_to_gcs_combined.py
@@ -45,7 +45,7 @@ def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
 @task
 def write_gcs(path: Path) -> None:
     """Upload local parquet file to GCS"""
-    gcs_block = GcsBucket.load("zoom-gcs")
+    gcs_block = GcsBucket.load("zoomcampgcsbucket")
     gcs_block.upload_from_path(from_path=path, to_path=path)
     return
 
