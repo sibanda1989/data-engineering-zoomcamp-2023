ba0f7a0ebf867b5e49e73faec41b9a2dfca41832
Hastings Sibanda <hastingssbnd5@gmail.com>
2023-03-04 10:04:58 +0200

    feat: change main etl function to match fhv data

--- week2_workflow_orchestration/etl_web_to_gcs.py
+++ week2_workflow_orchestration/etl_web_to_gcs.py
@@ -50,8 +50,8 @@ def etl_web_to_gcs() -> None:
     color = "yellow"
     year = 2021
     month = 1
-    dataset_file = f"{color}_tripdata_{year}-{month:02}"
-    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+    dataset_file = f"fhv_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/fhv/{dataset_file}.csv.gz"
 
     df = fetch(dataset_url)
     df_clean = clean(df)
