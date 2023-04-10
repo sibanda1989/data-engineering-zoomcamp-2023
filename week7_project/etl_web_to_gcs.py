58d115a97aa138a5657196bb4c84881e4077415c
Hastings Sibanda <hastingssbnd5@gmail.com>
2023-04-10 08:43:29 +0200

    feat: write function to upload data to gcs

--- week7_project/etl_web_to_gcs.py
+++ week7_project/etl_web_to_gcs.py
@@ -13,11 +13,25 @@ def fetch_url_data(url):
     data = r.json()
     return data
 
+@task
 def convert(key) -> pd.DataFrame:
     """Convert JSON data to pandas DataFrame"""
     df = pd.DataFrame(json[key])
     return df
 
+@task
+def write_local(df:pd.DataFrame, key:str) -> Path:
+    Path(("fpl").mkdir(parents=True, exist_ok=True))
+    path = Path(f"data/{key}.parquet")
+    df.to_parquet(path, compression="gzip")
+    return path
+
+@task
+def write_gcs(path: Path) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("zoomcampgcsbucket")
+    gcs_block.upload_from_path(from_path=path, to_path=path, timeout=1000)
+    return
 
 @flow
 def etl_web_to_gcs() -> None:
@@ -25,4 +39,7 @@ def etl_web_to_gcs() -> None:
     url = "https://fantasy.premierleague.com/api/bootstrap-static/"
     json_data = fetch_url_data(url)
     for key in json_data.keys():
-        path = convert(key)
+        df = convert(key)
+        path = write_local(df, key)
+        write_gcs(path)
+
